386893db418dc5935e5132037747ea65ee88ae61
Krishnavamsi <111332913+krishnavamsi-22@users.noreply.github.com>
2023-12-23 23:07:34 +0530

    Arrays.js

---
+++ Day 3/Arrays.js
@@ -0,0 +1,6 @@
+function getSecondLargest(nums) {
+    // Complete the function
+    const sorted_array          = nums.sort((a,b) => a - b );
+    const unique_sorted_array   = [...new Set(sorted_array)];
+    return unique_sorted_array[unique_sorted_array.length - 2];
+}
